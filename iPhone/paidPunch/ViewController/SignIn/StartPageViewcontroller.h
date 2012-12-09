5f0650deeb2759cd62d97167e18d3896d0493ed7
Aaron Khoo <aaron.khoo@geolopigs.com>
2012-12-08 19:01:08 -0800

    Missed some files in the last checkin

---
+++ iPhone/paidPunch/ViewController/SignIn/StartPageViewcontroller.h
@@ -0,0 +1,42 @@
+//
+//  StartPageViewcontroller.h
+//  paidPunch
+//
+//  Created by Alexander Nabavi-Noori on 7/26/12.
+//  Copyright (c) 2012 mobimedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DualSignInViewController.h"
+#import "CustomMoviePlayer.h"
+#import "UsingPaidPunchViewController.h"
+#import "FAQViewController.h"
+
+@interface StartPageViewController : UIViewController <UIScrollViewDelegate,UITextFieldDelegate>
+{
+	UIScrollView* scrollView;
+	UIPageControl* pageControl;
+	
+	BOOL pageControlBeingUsed;
+    
+    NSTimer     *pagingTimer;
+    BOOL        userHasInteracted;
+    
+    int totalContentCount;
+}
+
+@property (nonatomic, retain) IBOutlet UIScrollView  *scrollView;
+@property (nonatomic, retain) IBOutlet UIPageControl *pageControl;
+
+@property (nonatomic, retain) NSMutableArray *viewControllers;
+
+
+- (IBAction)changePage:(id)sender;
+
+- (IBAction)showFAQ:(id)sender;
+- (IBAction)pressPlay:(id)sender;
+- (IBAction)signIn:(id)sender;
+
+- (void)autoChangePage;
+
+@end
