45bdfa6c49a2b623c308bc86311a161640172322
Aaron Khoo <aaron.khoo@geolopigs.com>
2013-02-14 23:27:34 -0800

    Fix a couple bugs in the VoteBusiness page
    1. Extended row by one so that user can scroll easily
    
    2. Enable logo pulled from server for each business

--- iPhone/paidPunch/ViewController/ProposedBusinessesView.m
+++ iPhone/paidPunch/ViewController/ProposedBusinessesView.m
@@ -10,6 +10,8 @@
 #import "ProposedBusiness.h"
 #import "ProposedBusinesses.h"
 #import "ProposedBusinessesView.h"
+#import "UrlImage.h"
+#import "UrlImageManager.h"
 
 static CGFloat const kRowHeight = 70.0;
 static CGFloat const kImageSize = 50.0;
@@ -24,6 +26,7 @@ static CGFloat const kVoteButtonWidth = 40.0;
     {
         self.dataSource = self;
         self.delegate = self;
+        [self setRowHeight:kRowHeight];
     }
     return self;
 }
@@ -37,100 +40,100 @@ static CGFloat const kVoteButtonWidth = 40.0;
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [[[ProposedBusinesses getInstance] proposedBusinessesArray] count];
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    return kRowHeight;
+    // Add one to the end so that it can be scrolled properly
+    return [[[ProposedBusinesses getInstance] proposedBusinessesArray] count] + 1;
 }
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"ProposedBusinessCell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil)
-    {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-    }
+{    
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
     
     [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
     
-    ProposedBusiness* current = [[[ProposedBusinesses getInstance] proposedBusinessesArray] objectAtIndex:indexPath.row];
-    
-    // Display business image
-    // TODO: Replace filepath with logopath
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"pp_icon" ofType:@"png"];
-    NSData *imageData = [NSData dataWithContentsOfFile:filePath];
-    UIImage *image = [[UIImage alloc] initWithData:imageData];
-    UIImageView *lbImgView = [[UIImageView alloc] initWithImage:image];
-    //set contentMode to scale aspect to fit
-    lbImgView.contentMode = UIViewContentModeScaleAspectFit;
-    //change width of frame
-    CGRect frame = lbImgView.frame;
-    frame.origin.x = 5.0;
-    frame.origin.y = (kRowHeight - kImageSize)/2;
-    frame.size.height = kImageSize;
-    frame.size.width = kImageSize;
-    lbImgView.frame = frame;
-    [cell.contentView addSubview:lbImgView];
-    
-    // Display business name
-    CGFloat textXPos = kImageSize + 20;
-    UIFont* name_font = [UIFont fontWithName:@"Helvetica" size:19.0f];
-    CGFloat constrainedSize = 265.0f;
-    NSString* nameText = [current name];
-    CGSize sizenameText = [nameText sizeWithFont:name_font
-                               constrainedToSize:CGSizeMake(constrainedSize, CGFLOAT_MAX)
-                                   lineBreakMode:UILineBreakModeWordWrap];
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(textXPos, 0.0, sizenameText.width, kRowHeight/2)];
-    label.text = nameText;
-    label.backgroundColor = [UIColor clearColor];
-    label.textColor = [UIColor blackColor];
-    [label setNumberOfLines:1];
-    [label setFont:name_font];
-    label.textAlignment = UITextAlignmentLeft;
-    [cell.contentView addSubview:label];
-    
-    // Display business desc
-    UIFont* desc_font = [UIFont fontWithName:@"Helvetica" size:13.0f];
-    NSString* descText = [current desc];
-    UILabel *descLabel = [[UILabel alloc] initWithFrame:CGRectMake(textXPos, kRowHeight/2, stdiPhoneWidth-kVoteButtonWidth - textXPos, kRowHeight/2)];
-    descLabel.text = descText;
-    descLabel.backgroundColor = [UIColor clearColor];
-    descLabel.textColor = [UIColor blackColor];
-    [descLabel setNumberOfLines:0];
-    [descLabel setFont:desc_font];
-    descLabel.textAlignment = UITextAlignmentLeft;
-    [descLabel sizeToFit];
-    [cell.contentView addSubview:descLabel];
-    
-    // Display voting button    
-    UIButton* voteButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [voteButton addTarget:self action:@selector(didPressVoteButton:event:) forControlEvents:UIControlEventTouchUpInside];
-    [voteButton setImage:[UIImage imageNamed:@"grey-vote.png"] forState:UIControlStateNormal];
-    [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateSelected];
-    [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateHighlighted];
-    [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateSelected | UIControlStateDisabled];
-    [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateDisabled];
-    [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateSelected | UIControlStateHighlighted];
-    if ([[ProposedBusinesses getInstance] alreadyVoted:[current proposedBusinessId]])
+    // The last row should be left blank
+    if (indexPath.row != [[[ProposedBusinesses getInstance] proposedBusinessesArray] count])
     {
-        [voteButton setSelected:TRUE];
-        voteButton.enabled = FALSE;
+        ProposedBusiness* current = [[[ProposedBusinesses getInstance] proposedBusinessesArray] objectAtIndex:indexPath.row];
+        
+        // Display business image
+        UIImageView *lbImgView = [[UIImageView alloc] initWithFrame:CGRectMake(5.0, (kRowHeight - kImageSize)/2, kImageSize, kImageSize)];
+        //set contentMode to scale aspect to fit
+        lbImgView.contentMode = UIViewContentModeScaleAspectFit;        
+        UrlImage* urlImage = [[UrlImageManager getInstance] getCachedImage:[current logoPath]];
+        if(urlImage)
+        {
+            if ([urlImage image])
+            {
+                [lbImgView setImage:[urlImage image]];
+            }
+            else
+            {
+                [urlImage addImageView:lbImgView];
+            }
+        }
+        else
+        {
+            UrlImage* urlImage = [[UrlImage alloc] initWithUrl:[current logoPath] forImageView:lbImgView];
+            [[UrlImageManager getInstance] insertImageToCache:[current logoPath] image:urlImage];
+        }
+        [cell.contentView addSubview:lbImgView];
+        
+        // Display business name
+        CGFloat textXPos = kImageSize + 20;
+        UIFont* name_font = [UIFont fontWithName:@"Helvetica" size:19.0f];
+        CGFloat constrainedSize = 265.0f;
+        NSString* nameText = [current name];
+        CGSize sizenameText = [nameText sizeWithFont:name_font
+                                   constrainedToSize:CGSizeMake(constrainedSize, CGFLOAT_MAX)
+                                       lineBreakMode:UILineBreakModeWordWrap];
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(textXPos, 0.0, sizenameText.width, kRowHeight/2)];
+        label.text = nameText;
+        label.backgroundColor = [UIColor clearColor];
+        label.textColor = [UIColor blackColor];
+        [label setNumberOfLines:1];
+        [label setFont:name_font];
+        label.textAlignment = UITextAlignmentLeft;
+        [cell.contentView addSubview:label];
+        
+        // Display business desc
+        UIFont* desc_font = [UIFont fontWithName:@"Helvetica" size:13.0f];
+        NSString* descText = [current desc];
+        UILabel *descLabel = [[UILabel alloc] initWithFrame:CGRectMake(textXPos, kRowHeight/2, stdiPhoneWidth-kVoteButtonWidth - textXPos, kRowHeight/2)];
+        descLabel.text = descText;
+        descLabel.backgroundColor = [UIColor clearColor];
+        descLabel.textColor = [UIColor blackColor];
+        [descLabel setNumberOfLines:0];
+        [descLabel setFont:desc_font];
+        descLabel.textAlignment = UITextAlignmentLeft;
+        [descLabel sizeToFit];
+        [cell.contentView addSubview:descLabel];
+        
+        // Display voting button
+        UIButton* voteButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [voteButton addTarget:self action:@selector(didPressVoteButton:event:) forControlEvents:UIControlEventTouchUpInside];
+        [voteButton setImage:[UIImage imageNamed:@"grey-vote.png"] forState:UIControlStateNormal];
+        [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateSelected];
+        [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateHighlighted];
+        [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateSelected | UIControlStateDisabled];
+        [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateDisabled];
+        [voteButton setImage:[UIImage imageNamed:@"orange-vote.png"] forState:UIControlStateSelected | UIControlStateHighlighted];
+        if ([[ProposedBusinesses getInstance] alreadyVoted:[current proposedBusinessId]])
+        {
+            [voteButton setSelected:TRUE];
+            voteButton.enabled = FALSE;
+        }
+        voteButton.contentMode = UIViewContentModeScaleAspectFit;
+        //change width of frame
+        CGRect voteFrame = voteButton.frame;
+        voteFrame.origin.x = stdiPhoneWidth - (kVoteButtonWidth + 10);
+        voteFrame.origin.y = (kRowHeight - kVoteButtonWidth)/2;
+        voteFrame.size.height = kVoteButtonWidth;
+        voteFrame.size.width = kVoteButtonWidth;
+        voteButton.frame = voteFrame;
+        [cell.contentView addSubview:voteButton];
     }
-    voteButton.contentMode = UIViewContentModeScaleAspectFit;
-    //change width of frame
-    CGRect voteFrame = voteButton.frame;
-    voteFrame.origin.x = stdiPhoneWidth - (kVoteButtonWidth + 10);
-    voteFrame.origin.y = (kRowHeight - kVoteButtonWidth)/2;
-    voteFrame.size.height = kVoteButtonWidth;
-    voteFrame.size.width = kVoteButtonWidth;
-    voteButton.frame = voteFrame;
-    [cell.contentView addSubview:voteButton];
     
     return cell;
 }
